9f21c1ce587d9eea0714e833f8b4fd2a5caeb404
sueroku <eraltndusn@gmail.com>
2024-08-28 12:03:11 +0900

    16th

--- src/main/java/com/beyond/ordersystem/common/config/CorsConfig.java
+++ src/main/java/com/beyond/ordersystem/common/config/CorsConfig.java
@@ -12,8 +12,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 어떤 url이든 허용
 //                .allowedOrigins("http://localhost:8081") // 허용 url 명시 (나중에 우리 서버에 들어올 url 여기에 추가)
-                // .allowedOrigins("https://www.suguri.store")
-                .allowedOrigins("http://www.suguri.store")
+                 .allowedOrigins("https://www.suguri.store")
+//                .allowedOrigins("http://www.suguri.store")
                 .allowedMethods("*") // 어떤 메서드든 허용
                 .allowedHeaders("*")
                 .allowCredentials(true); // 보안처리 할거냐 말거냐
